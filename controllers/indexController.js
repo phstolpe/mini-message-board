f49e26febbb2dec510281079c3ea9fcc7b9739b4
phstolpe <123520279+phstolpe@users.noreply.github.com>
2026-03-18 16:29:09 +0100

    feat: add input validation to form

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,4 +1,5 @@
 const db = require("../db/queries");
+const { validationResult } = require("express-validator");
 
 async function getMessages(req, res) {
 	const messages = await db.getAllMessages();
@@ -10,6 +11,11 @@ function getForm(req, res) {
 }
 
 async function createMessage(req, res) {
+	const errors = validationResult(req);
+	if (!errors.isEmpty()) {
+		return res.status(404).render("form", { errors: errors.array() });
+	}
+
 	const user = req.body.user;
 	const text = req.body.text;
 	await db.createMessage(user, text);
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,12 +1,18 @@
 const { Router } = require("express");
+const { body } = require("express-validator");
 
 const indexRouter = Router();
 const indexController = require("../controllers/indexController.js");
+
+const validateMessage = [
+	body("name").trim().isAlpha().withMessage("Name must contain letters only.").isLength({ min: 3, max: 15 }).withMessage("Must be between 3 and 15 characters"),
+	body("message").trim().isLength({ min: 1, max: 200 }).withMessage("Message length must be between 1 and 200 characters")
+];
 
 
 indexRouter.get("/", indexController.getMessages);
 indexRouter.get("/new", indexController.getForm);
 
-indexRouter.post("/new", indexController.createMessage);
+indexRouter.post("/new", validateMessage, indexController.createMessage);
 
 module.exports = indexRouter;
